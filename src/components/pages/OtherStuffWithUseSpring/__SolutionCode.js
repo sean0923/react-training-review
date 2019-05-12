8407c555d4721f7fa79591498e69decf10ee4de9
Sean <iamseanhong@gmail.com>
2019-05-11 23:52:27 -0600

    Add margin to hr

--- src/components/pages/OtherStuffWithUseSpring/__SolutionCode.js
+++ src/components/pages/OtherStuffWithUseSpring/__SolutionCode.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useSpring, animated } from 'react-spring';
 
 const SolutionCode = () => {
+  const [toggleColor, setToggleColor] = useState(false);
+
+  const colorChange = useSpring(toggleColor ? { color: 'tomato' } : { color: 'green' });
+
   return (
     <div>
-      <div>SolutionCode</div>
+      <animated.h1 style={colorChange}>Color</animated.h1>
+      <button onClick={() => setToggleColor(!toggleColor)}>toggleColor</button>
+      <hr />
+
+      <animated.h1 style={colorChange}>Color</animated.h1>
+      <button onClick={() => setToggleColor(!toggleColor)}>toggleColor</button>
+      <hr />
     </div>
   );
 };
