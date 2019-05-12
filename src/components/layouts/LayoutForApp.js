090a8e09a2cef33084809f4d91497fa602d939aa
Sean <iamseanhong@gmail.com>
2019-05-12 16:03:48 -0600

    Toggle navbar when less than tablet"

--- src/components/layouts/LayoutForApp.js
+++ src/components/layouts/LayoutForApp.js
@@ -1,5 +1,7 @@
-import React from 'react';
-import styled, { css } from 'styled-components';
+import React, { useState } from 'react';
+import styled from 'styled-components/macro';
+import { MdClose, MdMenu } from 'react-icons/md';
+
 import media, { LESS_THAN_TABLET } from '../../styled-component-helpers/media';
 
 // -----------------------------------------------------------------------------------------
@@ -9,14 +11,20 @@ import media, { LESS_THAN_TABLET } from '../../styled-component-helpers/media';
 import Navbar from './layoutForApp/Navbar';
 
 const LayoutForApp = ({ children }) => {
+  const [isNavbarHidden, setIsNavbarHidden] = useState(true);
+
   return (
     <div>
-      <NavbarWrapper>
+      <NavbarWrapper isNavbarHidden={isNavbarHidden}>
         <Navbar />
       </NavbarWrapper>
       <Wrapper>
-        <NavbarPlaceHolder />
-        <BodyWrapper>
+        <NavbarPlaceHolder isNavbarHidden={isNavbarHidden} />
+        <BodyWrapper isNavbarHidden={isNavbarHidden}>
+          <NavbarToggleBtn onClick={() => setIsNavbarHidden(!isNavbarHidden)}>
+            {isNavbarHidden ? <MdMenu size="100%" /> : <MdClose size="100%" />}
+          </NavbarToggleBtn>
+
           <Body>{children}</Body>
         </BodyWrapper>
       </Wrapper>
@@ -29,10 +37,11 @@ const LayoutForApp = ({ children }) => {
 // -----------------------------------------------------------------------------------------
 const navbarWidth = '220px';
 
-const hideNavbar = css`
+const hideNavbar = `
   transform: translateX(-100%);
 `;
-const hideNavbarPlaceHolder = css`
+
+const hideNavbarPlaceHolder = `
   transform: translateX(-100%);
   width: 0;
 `;
@@ -43,14 +52,18 @@ const NavbarWrapper = styled.div`
   position: fixed;
   width: ${navbarWidth};
   ${transition}
-  ${media[LESS_THAN_TABLET](hideNavbar)};
+  ${media[LESS_THAN_TABLET]`
+    ${(props) => props.isNavbarHidden && hideNavbar}
+  `};
 `;
 
 const NavbarPlaceHolder = styled.div`
   width: ${navbarWidth};
   height: 100vh;
   ${transition}
-  ${media[LESS_THAN_TABLET](hideNavbarPlaceHolder)};
+  ${media[LESS_THAN_TABLET]`
+    ${(props) => props.isNavbarHidden && hideNavbarPlaceHolder}
+  `};
 `;
 
 const Wrapper = styled.div`
@@ -58,13 +71,37 @@ const Wrapper = styled.div`
 `;
 
 const BodyWrapper = styled.div`
+  position: relative;
   width: calc(100vw - ${navbarWidth});
-  ${media[LESS_THAN_TABLET]`width: 100vw;`};
+
+  ${media[LESS_THAN_TABLET]`
+    ${(props) => props.isNavbarHidden && 'width: 100vw;'}
+  `};
 `;
 
 const Body = styled.div`
   width: 80%;
   margin: 3rem auto;
+`;
+
+const NavbarToggleBtn = styled.div`
+  cursor: pointer;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 2rem;
+  height: 2rem;
+  background-color: var(--color-transparent-dark-2);
+
+  visibility: hidden;
+  opacity: 0;
+
+  ${transition}
+
+  ${media[LESS_THAN_TABLET]`
+    visibility: visible;
+    opacity: 1;
+  `};
 `;
 
 export default LayoutForApp;
