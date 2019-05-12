09d26922468a28b7e4473be056a212fbc42e57d5
Sean <iamseanhong@gmail.com>
2019-05-12 16:43:51 -0600

    Able to put children in between

--- src/components/pages/LegoPage/Lego.js
+++ src/components/pages/LegoPage/Lego.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Lego = ({ color = 'red' }) => {
-  return <Wrapper color={color} />;
+const Lego = ({ color = 'red', children }) => {
+  return <Wrapper color={color}>{children}</Wrapper>;
 };
 
 export default Lego;
@@ -14,5 +14,12 @@ const Wrapper = styled.div`
   background-color: ${(props) => props.color && props.color};
   border: 0.5rem solid var(--color-black);
 
+  padding: 1rem;
+
   min-height: 2rem;
+  font-size: 1.5rem;
+  font-weight: bold;
+  color: white;
+
+  text-align: center;
 `;
--- src/components/pages/LegoPage/SolutionCode.js
+++ src/components/pages/LegoPage/SolutionCode.js
@@ -8,10 +8,11 @@ import Lego from './Lego';
 const SolutionCode = () => {
   return (
     <div>
-      <Lego color="pink" />
-      <Lego color="blue" />
-      <Lego color="green" />
-      <Lego color="red" />
+      <Lego color="pink">pink</Lego>
+      <Lego color="blue">blue</Lego>
+      <Lego color="green">green</Lego>
+      <Lego color="red">red</Lego>
+      <Lego color="var(--color-black)">Put anything you want</Lego>
     </div>
   );
 };
