69fe96a7b6dd299b183c3743d90cea186a31dd1c
Sean <iamseanhong@gmail.com>
2019-05-13 19:52:26 -0700

    Add Functional Component

--- src/data/dataForRoutes.js
+++ src/data/dataForRoutes.js
@@ -4,24 +4,18 @@
 import Home from '../components/pages/Home/Home';
 
 import LegoPage from '../components/pages/LegoPage/LegoPage';
-import ShowAndHideWithUseSpring from '../components/pages/ShowAndHideWithUseSpring/ShowAndHideWithUseSpring';
-import OtherStuffWithUseSpring from '../components/pages/OtherStuffWithUseSpring/OtherStuffWithUseSpring';
-import PageNavbar from '../components/pages/PageNavbar/PageNavbar';
+import FunctionalComponent from '../components/pages/FunctionalComponent/FunctionalComponent';
 
 export const routeKeys = {
   HOME: 'HOME',
   LegoPage: 'LegoPage',
-  ShowAndHideWithUseSpring: 'ShowAndHideWithUseSpring',
-  OtherStuffWithUseSpring: 'OtherStuffWithUseSpring',
-  PageNavbar: 'PageNavbar',
+  FunctionalComponent: 'FunctionalComponent',
 };
 
 export const components = {
   [routeKeys.HOME]: Home,
   [routeKeys.LegoPage]: LegoPage,
-  [routeKeys.ShowAndHideWithUseSpring]: ShowAndHideWithUseSpring,
-  [routeKeys.OtherStuffWithUseSpring]: OtherStuffWithUseSpring,
-  [routeKeys.PageNavbar]: PageNavbar,
+  [routeKeys.FunctionalComponent]: FunctionalComponent,
 };
 
 const getLink = (routeKey) => {
