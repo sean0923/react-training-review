978cc5780c92725c61458b148d36309a3a62cd4b
Sean <iamseanhong@gmail.com>
2019-05-13 20:27:32 -0700

    Functional component solution code

--- src/components/pages/FunctionalComponent/SolutionCode.js
+++ src/components/pages/FunctionalComponent/SolutionCode.js
@@ -1,9 +1,51 @@
 import React from 'react';
+import userOneData from './userOneData.json';
 
-const SolutionCode = () => {
+const GeneralInfo = ({ name, username, email, phone, website }) => {
   return (
     <div>
-      <div>SolutionCode</div>
+      <hr />
+      <h3>General Info</h3>
+      <hr />
+
+      <ul>
+        <li>{`name: ${name}`}</li>
+        <li>{`username: ${username}`}</li>
+        <li>{`email: ${email}`}</li>
+        <li>{`phone: ${phone}`}</li>
+        <li>{`website: ${website}`}</li>
+      </ul>
+    </div>
+  );
+};
+
+const Address = ({ address }) => {
+  const { street, suite, city, zipcode } = address;
+  return (
+    <div>
+      <hr />
+      <h3>Address</h3>
+      <hr />
+
+      <ul>
+        <li>{`street: ${street}`}</li>
+        <li>{`suite: ${suite}`}</li>
+        <li>{`city: ${city}`}</li>
+        <li>{`zipcode: ${zipcode}`}</li>
+      </ul>
+    </div>
+  );
+};
+
+const SolutionCode = () => {
+  const { name, username, email, address, phone, website } = userOneData;
+
+  return (
+    <div>
+      <h2>User Info</h2>
+
+      <GeneralInfo name={name} username={username} email={email} phone={phone} website={website} />
+      <Address address={address} />
     </div>
   );
 };
